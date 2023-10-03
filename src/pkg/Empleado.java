55c1c9d81efd5bd7284cffc113cc6ad9223cb14f
elarg <elarg@Vctor>
2023-10-03 17:33:03 +0200

    actualizacion

--- src/pkg/Empleado.java
+++ src/pkg/Empleado.java
@@ -1,5 +1,34 @@
 package pkg;
 
 public class Empleado {
+		public enum TipoEmpleado{Vendedor,Encargado};
+		public float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) {
+			float salarioBase=0, primas=0,extras=0;
+			if(tipo==TipoEmpleado.Encargado) {
+				salarioBase=2500;
+				
+			}else if(tipo==TipoEmpleado.Vendedor) {
+				salarioBase=2000;
+			}
+			if(ventasMes>1500) {
+				primas=200;
+			}else if(ventasMes>=1000) {
+				primas=100;	
+			}
+			extras=horasExtra*30;
+			return salarioBase+primas+extras;
+		} 
 
+
+	public float calculoNominaNeta(float nominaBruta) {
+		float retencion=0;
+		if(nominaBruta>2500) {
+			retencion=0.18f;
+			
+		}else if(nominaBruta>2000) {
+			retencion=0.15f;
+		}
+		return nominaBruta*(1-retencion);
+		
+	}
 }
---
+++ src/test/EmpleadoTest.java
@@ -0,0 +1,93 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EmpleadoTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCalculoNominaBruta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCalculoNominaNeta() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testCalculoNominaBrutaEncargado() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testCalculoNominaBrutaVendedor() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testCalculoNominaBrutaOtro() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testCalculoNominaBrutaVentaMesNegativa() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testCalculoNominaBrutaVentaMesMenorMil() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testCalculoNominaBrutaVentaMesEntre1000y1500(){
+		fail("Not yet implemented");
+	}
+	@Test
+	void testCalculoNominaBrutaVentaMesMayor2500() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testCalculoNominaBrutaHorasExtrasPositivas() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testCalculoNominaBrutaHorasExtrasNegativas() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testCalculoNominaNetaNegativa() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testCalculoNominaNetaMenos2100() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testCalculoNominaNetaEntre2100y2500() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testCalculoNominaNetaMayor2500() {
+		fail("Not yet implemented");
+	}
+	
+
+}
