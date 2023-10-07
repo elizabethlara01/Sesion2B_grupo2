09a1b79832bf316cf1ac49b6389c81247e81bad0
elarg <elarg@Vctor>
2023-10-07 14:54:11 +0200

    terminado

--- src/test/EmpleadoTest.java
+++ src/test/EmpleadoTest.java
@@ -12,8 +12,9 @@ import pkg.Empleado;
 import pkg.Empleado.TipoEmpleado;
 
 class EmpleadoTest {
-	float ventaMes;
-	float horasExtras;
+	float ventaMes,ventaMesNegativo;
+	float horasExtras,horasExtrasNegativo;
+	float nominaBruta;
 	TipoEmpleado encargado, vendedor; 
 	
 	@BeforeAll
@@ -27,7 +28,9 @@ class EmpleadoTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		ventaMes=0;
+		ventaMesNegativo=-1;
 		horasExtras=0;
+		horasExtrasNegativo=-1;
 		encargado=TipoEmpleado.Encargado;
 		vendedor=TipoEmpleado.Vendedor;
 		
@@ -73,61 +76,69 @@ class EmpleadoTest {
 	@Test
 	void testCalculoNominaBrutaVentaMesNegativa() {
 		float expected=-1;
-		float actual=Empleado.calculoNominaBruta(encargado, -1, horasExtras);
+		float actual=Empleado.calculoNominaBruta(encargado, ventaMesNegativo, horasExtras);
 		assertEquals(expected, actual);
 	}
 	@Test
 	void testCalculoNominaBrutaVentaMesMenorMil() {
+		ventaMes=999;
 		float expected=2000;
-		float actual=Empleado.calculoNominaBruta(vendedor, 999, horasExtras);
+		float actual=Empleado.calculoNominaBruta(vendedor, ventaMes, horasExtras);
 		assertEquals(expected, actual);
 	}
 	@Test
 	void testCalculoNominaBrutaVentaMesEntre1000y1500(){
+		ventaMes=1000;
 		float expected=2600;
-		float actual=Empleado.calculoNominaBruta(encargado, 1000,horasExtras);
+		float actual=Empleado.calculoNominaBruta(encargado, ventaMes,horasExtras);
 		assertEquals(expected,actual);
 	}
 	@Test
 	void testCalculoNominaBrutaVentaMesMayor2500() {
+		ventaMes=1600;
 		float expected=2200;
-		float actual=Empleado.calculoNominaBruta(vendedor, 1600, horasExtras);
+		float actual=Empleado.calculoNominaBruta(vendedor, ventaMes, horasExtras);
 		assertEquals(expected,actual);
 	}
 	@Test
 	void testCalculoNominaBrutaHorasExtrasPositivas() {
+		horasExtras=1;
 		float expected=2530;
-		float actual=Empleado.calculoNominaBruta(encargado, 0, 1);
+		float actual=Empleado.calculoNominaBruta(encargado, ventaMes, horasExtras);
 		assertEquals(expected,actual);
 	}
 	@Test
 	void testCalculoNominaBrutaHorasExtrasNegativas() {
 		float expected=-1;
-		float actual=Empleado.calculoNominaBruta(encargado, 0, -1);
+		float actual=Empleado.calculoNominaBruta(encargado, ventaMes, horasExtrasNegativo);
 		assertEquals(expected,actual);
 	}
 	@Test
 	void testCalculoNominaNetaNegativa() {
+		nominaBruta=-1;
 		float expected=-1;
-		float actual=Empleado.calculoNominaNeta(-1);
+		float actual=Empleado.calculoNominaNeta(nominaBruta);
 		assertEquals(expected,actual);
 	}
 	@Test
 	void testCalculoNominaNetaMenos2100() {
+		nominaBruta=2000;
 		float expected=2000;
-		float actual=Empleado.calculoNominaNeta(2000);
+		float actual=Empleado.calculoNominaNeta(nominaBruta);
 		assertEquals(expected, actual);
 	}
 	@Test
 	void testCalculoNominaNetaEntre2100y2500() {
+		nominaBruta=2200;
 		float expected=1870;
-		float actual=Empleado.calculoNominaNeta(2200);
+		float actual=Empleado.calculoNominaNeta(nominaBruta);
 		assertEquals(expected,actual);
 	}
 	@Test
 	void testCalculoNominaNetaMayor2500() {
+		nominaBruta=2600;
 		float expected=2132;
-		float actual=Empleado.calculoNominaNeta(2600);
+		float actual=Empleado.calculoNominaNeta(nominaBruta);
 		assertEquals(expected,actual);
 	}
 
